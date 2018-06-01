9c0aa16971bca406a46c16b8cdc4a6d6de0b21b1
EliottCH <38426411+EliottCH@users.noreply.github.com>
2018-06-01 08:37:37 +0200

    Update oversized-shift.c

--- examples/04-Oversized-Shift/oversized-shift.c
+++ examples/04-Oversized-Shift/oversized-shift.c
@@ -1,24 +1,14 @@
+typdef struct {
+   int x;
+   int y;
+} ms;
 
-int main(void)
+intint main(int a, int b)
 {
-    int i, sum;
-
-    sum = 2;
-    for (i = 0; i <= 4; i++)
-        /* TrustInSoft Analyzer emits an alarm concerning the left-shift
-           expression '2 << i'. In particular, the analyzer is not able to
-           guarantee '0 ≤ (int)(2<<i) < 32' valid (on x86 architecture).
-
-           Note that, as soon as the variable 'i' takes the value 5, the
-           left-shift expression '2 << i' evaluates to 32. Hence, '1 << (2 <<
-           i)' reduces to '1 << 32'.
-
-           According to section 6.5.7 of the C11 Standard, the behavior of '1 <<
-           32' is undefined. This is because, on x86 architecture, the right
-           operand 32 is greater than or equal to the width (in bits) of the
-           left operand 1.
-        */
-      sum += 1 << (2 << i);
-
+    ms var[5];
+    ms (*ptr)[]=&var;
+    var[0].x=a;
+    var[0].y=b;
+    //@ assert (*ptr)[0].x == a;
     return 0;
 }
